382d3e72fc88d92191b9215c471f4c3e8558aa50
ye8um <ybyoo0715@gmail.com>
2026-03-31 22:01:45 -0400

    Add hackpad project files

---
+++ firmware/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define ENCODERS_PAD_A { F4 }  
+#define ENCODERS_PAD_B { F5 } 
+#define ENCODER_RESOLUTION 4
---
+++ firmware/keymaps/default/keymap.c
@@ -0,0 +1,21 @@
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+    _BASE,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT_1x3_enc(
+        KC_A, KC_B, KC_C
+    )
+};
+
+// THIS replaces encoder_map
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return true;
+}
